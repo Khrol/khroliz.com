cbf9ea11a1491c62a3a3ac5762f4fc719d609b58
Igor Khrol <khroliz@gmail.com>
2018-10-11 09:58:59 +0300

    Run ESlint on Travis

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
-library.add(fab, faEnvelope)
+library.add(fab, faEnvelope);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
